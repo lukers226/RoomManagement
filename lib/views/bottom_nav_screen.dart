c76495b45c4ab5c669b3254451b3845c8f2df3fa
KAVI7871281698 <kavipriyan292004@gmail.com>
2026-01-04 20:33:26 +0530

    modify the buttom nav bar

--- lib/views/bottom_nav_screen.dart
+++ lib/views/bottom_nav_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
+
 import 'home_view.dart';
 import 'spent_view.dart';
 import 'members_view.dart';
 import 'settings_view.dart';
 import '../app_colors.dart';
+import '../widgets/svg.dart';
 
 class BottomNavScreen extends StatefulWidget {
   const BottomNavScreen({super.key});
@@ -17,82 +17,81 @@ class BottomNavScreen extends StatefulWidget {
 class _BottomNavScreenState extends State<BottomNavScreen> {
   int _selectedIndex = 0;
 
-  static const List<Widget> _widgetOptions = <Widget>[
+  final List<Widget> _screens = const [
     HomeView(),
     SpentView(),
     MembersView(),
     SettingsView(),
   ];
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+  /// ✅ ICON + UNDERLINE (NO COLOR ON IMAGE)
+  Widget navItem(String asset, int index) {
+    final bool isSelected = _selectedIndex == index;
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          asset,
+          height: 24,
+          width: 24,
+          fit: BoxFit.contain,
+        ),
+        const SizedBox(height: 6),
+
+        // ✅ UNDERLINE INDICATOR
+        AnimatedContainer(
+          duration: const Duration(milliseconds: 250),
+          height: 3,
+          width: isSelected ? 24 : 0,
+          decoration: BoxDecoration(
+            color: isSelected
+                ? AppColors.secondaryColor
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(4),
+          ),
+        ),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-
-    // Responsive helpers
-    double w(double v) => size.width * v;
-    double h(double v) => size.height * v;
-    double sp(double v) => size.width * v;
-
     return Scaffold(
-      body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          color: AppColors.white,
-          boxShadow: [
-            BoxShadow(
-              blurRadius: 20,
-              color: Colors.black.withValues(alpha: 0.1),
-            )
-          ],
-        ),
-        child: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: w(0.04),
-              vertical: h(0.01),
-            ),
-            child: GNav(
-              rippleColor: AppColors.secondaryColor.withAlpha(1),
-              hoverColor: AppColors.secondaryColor.withAlpha(1),
-              gap: w(0.02),
-              activeColor: AppColors.white,
-              iconSize: sp(0.06),
-              padding: EdgeInsets.symmetric(
-                horizontal: w(0.05),
-                vertical: h(0.015),
-              ),
-              duration: const Duration(milliseconds: 400),
-              tabBackgroundColor: AppColors.secondaryColor,
-              color: Colors.grey,
-              tabs: [
-                GButton(
-                  icon: Icons.home,
-                  text: 'Home',
-                ),
-                GButton(
-                  icon: Icons.money_off,
-                  text: 'Spent',
-                ),
-                GButton(
-                  icon: Icons.group,
-                  text: 'Members',
-                ),
-                GButton(
-                  icon: Icons.settings,
-                  text: 'Settings',
-                ),
-              ],
-              selectedIndex: _selectedIndex,
-              onTabChange: _onItemTapped,
-            ),
+      body: _screens[_selectedIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: AppColors.white,
+        selectedItemColor: AppColors.secondaryColor, // text only
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        elevation: 8,
+
+        items: [
+          BottomNavigationBarItem(
+            icon: navItem(SvgManager.home, 0),
+            label: 'Home',
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: navItem(SvgManager.wallet, 1),
+            label: 'Spent',
+          ),
+          BottomNavigationBarItem(
+            icon: navItem(SvgManager.members, 2),
+            label: 'Members',
+          ),
+          BottomNavigationBarItem(
+            icon: navItem(SvgManager.members, 3),
+            label: 'Settings',
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/svg.dart
+++ lib/widgets/svg.dart
@@ -6,4 +6,8 @@ class SvgManager {
   static const String room = '$assetsPath/room.svg';
   static const String category = '$assetsPath/category.svg';
   static const String money = '$assetsPath/money.svg';
+  static const String home = 'assets/icons/home.png';
+  static const String wallet = 'assets/icons/wallet.png';
+  static const String members = 'assets/icons/group.png';
 }
+
